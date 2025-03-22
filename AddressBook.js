27382de6c8e822cb8e00e4c6291a77b1ced53a43
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-22 11:03:12 +0530

    Ability to delete is added

--- AddressBook.js
+++ AddressBook.js
@@ -1,4 +1,3 @@
-
 //UC1
 class Contact{
     firstName;
@@ -111,5 +110,20 @@ function editContact(findName,editedVariable,variableNewValue){
                 }
             }
         })
+    }
+}
+
+//UC5
+//method to delete
+function deleteContact(first_Name){
+    if(addressBook.length == null){
+        console.log("Add Contact In Address Book");
+    }else{
+        for(let i = 0; i <addressBook.length ; i++){
+            if(addressBook[i].firstName == first_Name){
+                addressBook.splice(i,1);
+                console.log("Contact Deleted Successfully");
+            }
+        }
     }
 }
